c758f322329360922af391590b326b67382cec9a
jaysonl <jaysonl@sxi.com.ph>
2020-05-10 09:09:03 +0800

    updates

--- src/main/java/com/project/user/MsUserApiApplication.java
+++ src/main/java/com/project/user/MsUserApiApplication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.project"})
 @EntityScan(basePackages = { 
-		"com.project.base.common.resource.client",
-		"com.project.base.common.resource.permission",
-		"com.project.base.common.resource.role",
-		"com.project.base.common.resource.user" })
+		"com.project.base.common.resources.client",
+		"com.project.base.common.resources.permission",
+		"com.project.base.common.resources.role",
+		"com.project.base.common.resources.user" })
 public class MsUserApiApplication {
 
 	public static void main(String[] args) {
